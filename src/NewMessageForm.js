4179446d1f78121a1a22190171af78ffbb03bb07
Lucas Alves <lcsjalves@gmail.com>
2023-06-02 19:48:54 -0300

    Include app screenshot and make input more visible

--- src/NewMessageForm.js
+++ src/NewMessageForm.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import {Button, Text, TextInput} from 'react-native';
+import {Button, View, TextInput} from 'react-native';
 
 export default function NewMessageForm({onSend}) {
   const [inputText, setInputText] = useState('');
@@ -10,13 +10,15 @@ export default function NewMessageForm({onSend}) {
   }
 
   return (
-    <>
+    <View style={{ margin: 15 }}>
       <TextInput
         testID="messageText"
         value={inputText}
         onChangeText={setInputText}
+        placeholder='Insert one thing...'
+        style={{ marginBottom: 10, borderWidth: 1, borderColor: '#ccc', padding: 10, fontSize: 18 }}
       />
       <Button title="Enviar" testID="sendButton" onPress={handleSend}></Button>
-    </>
+    </View>
   );
 }
